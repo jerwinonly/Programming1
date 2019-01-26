d9d034338db5bfdcc18d23a8488213cd62502e85
jerwinonly <47052777+jerwinonly@users.noreply.github.com>
2019-01-26 17:44:52 +0800

    Create loops.c

---
+++ loops.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+    int x=0;
+    while(x<100){
+        printf("%i\n",x);
+        x++;
+    }
+
+   int y=0;
+   do{
+       printf("%i\n",y);
+       y++;
+   }while(y<100);
+  
+    for(int z=0;z<100;z++){
+        printf("%i\n",z);
+    }
+    return 0;
+}
