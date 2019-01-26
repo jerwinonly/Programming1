37b99b73a9c6f49fd4ce4f9a06f5712630c1d0a0
jerwinonly <47052777+jerwinonly@users.noreply.github.com>
2019-01-26 16:29:02 +0800

    Create Conditionals.c
    Created Conditionals

---
+++ Conditionals.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    int baon = 0;
+    if(baon >=170){
+      printf("Pasok ako");
+}
+    else if(baon >60){
+      printf("Pwede na");
+}
+    else {
+    printf("Ayaw");
+    
+    }
+
+    return 0;
+}
